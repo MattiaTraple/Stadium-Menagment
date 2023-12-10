9ae39110cc8a0472ee6fd6539ddb694bf0c0a3f6
Agrin <agrins@metropolia.fi>
2023-12-10 13:42:09 +0200

    Added one more security for the vip

--- Stadium-Managing/src/main/java/simu/model/MyEngine.java
+++ Stadium-Managing/src/main/java/simu/model/MyEngine.java
@@ -4,6 +4,7 @@ import controller.IControllerForM;
 import eduni.distributions.Negexp;
 import eduni.distributions.Normal;
 import entity.ResultDb;
+import javafx.application.Application;
 import simu.framework.Clock;
 import simu.framework.IEngine;
 import simu.framework.Engine;
@@ -19,7 +20,7 @@ import java.util.Calendar;
 import java.util.Date;
 
 
-public class MyEngine extends Engine{
+public class MyEngine extends Engine {
 
     private ArrivalProcess arrivalProcess;
 
@@ -44,25 +45,26 @@ public class MyEngine extends Engine{
         servicePoints[1]=new Security(new Normal(10,6), eventList, EventType.SECURITY2, 265, 423);
         servicePoints[2]=new Security(new Normal(10,6), eventList, EventType.SECURITY3, 328, 430);
         servicePoints[3]=new Security(new Normal(10,6), eventList, EventType.SECURITY4, 270, 457);
+        servicePoints[4]=new Security(new Normal(10,6), eventList, EventType.SECURITY5, 270, 457);
 
-        servicePoints[4]=new Ticket(new Normal(10,6), eventList, EventType.TICKET_SELL1, 263, 360);
-        servicePoints[5]=new Ticket(new Normal(10,6), eventList, EventType.TICKET_SELL2, 234, 373);
-        servicePoints[6]=new Ticket(new Normal(10,6), eventList, EventType.TICKET_SELL3, 201, 389);
-        servicePoints[7]=new Ticket(new Normal(10,6), eventList, EventType.TICKET_SELL4, 166, 404);
+        servicePoints[5]=new Ticket(new Normal(10,6), eventList, EventType.TICKET_SELL1, 263, 360);
+        servicePoints[6]=new Ticket(new Normal(10,6), eventList, EventType.TICKET_SELL2, 234, 373);
+        servicePoints[7]=new Ticket(new Normal(10,6), eventList, EventType.TICKET_SELL3, 201, 389);
+        servicePoints[8]=new Ticket(new Normal(10,6), eventList, EventType.TICKET_SELL4, 166, 404);
 
-        servicePoints[8]=new CheckIn(new Normal(10,6), eventList, EventType.CHECK_IN1, 308, 524);
-        servicePoints[9]=new CheckIn(new Normal(10,6), eventList, EventType.CHECK_IN2, 324, 517);
-        servicePoints[10]=new CheckIn(new Normal(10,6), eventList, EventType.CHECK_IN3, 342, 505);
-        servicePoints[11]=new CheckIn(new Normal(10,6), eventList, EventType.CHECK_IN4, 387, 481);
-        servicePoints[12]=new CheckIn(new Normal(10,6), eventList, EventType.CHECK_IN5, 387 , 481);
+        servicePoints[9]=new CheckIn(new Normal(10,6), eventList, EventType.CHECK_IN1, 308, 524);
+        servicePoints[10]=new CheckIn(new Normal(10,6), eventList, EventType.CHECK_IN2, 324, 517);
+        servicePoints[11]=new CheckIn(new Normal(10,6), eventList, EventType.CHECK_IN3, 342, 505);
+        servicePoints[12]=new CheckIn(new Normal(10,6), eventList, EventType.CHECK_IN4, 387, 481);
+        servicePoints[13]=new CheckIn(new Normal(10,6), eventList, EventType.CHECK_IN5, 387 , 481);
 
-        servicePoints[13]=new Catering(new Normal(10,6), eventList, EventType.CATERING1, 321, 250);
-        servicePoints[14]=new Catering(new Normal(10,6), eventList, EventType.CATERING2, 201, 389);
-        servicePoints[15]=new Catering(new Normal(10,6), eventList, EventType.CATERING3, 166, 404);
-        servicePoints[16]=new Catering(new Normal(10,6), eventList, EventType.CATERING4, 134, 422);
+        servicePoints[14]=new Catering(new Normal(10,6), eventList, EventType.CATERING1, 321, 250);
+        servicePoints[15]=new Catering(new Normal(10,6), eventList, EventType.CATERING2, 201, 389);
+        servicePoints[16]=new Catering(new Normal(10,6), eventList, EventType.CATERING3, 166, 404);
+        servicePoints[17]=new Catering(new Normal(10,6), eventList, EventType.CATERING4, 134, 422);
 
-        servicePoints[17]=new NormalCustomer(new Normal(10,6), eventList, EventType.CUSTOMER_ARRIVAL, 257, 157);
-        servicePoints[18]=new VipCustomer(new Normal(10,6), eventList, EventType.VIP_CUSTOMER_ARRIVAL, 51, 359);
+        servicePoints[18]=new NormalCustomer(new Normal(10,6), eventList, EventType.CUSTOMER_ARRIVAL, 257, 157);
+        servicePoints[19]=new VipCustomer(new Normal(10,6), eventList, EventType.VIP_CUSTOMER_ARRIVAL, 51, 359);
 
         arrivalProcess = new ArrivalProcess(new Negexp(15,5), eventList, EventType.ARR1);
 
@@ -136,7 +138,7 @@ public class MyEngine extends Engine{
                     }
                     j++;
                 }} else {
-                    servicePoints[8].AddToTheLine(a);
+                    servicePoints[4].AddToTheLine(a);
                 }
                 break;
             case SECURITY2:
@@ -156,7 +158,7 @@ public class MyEngine extends Engine{
                         }
                         j++;
                     }} else {
-                    servicePoints[8].AddToTheLine(a);
+                    servicePoints[4].AddToTheLine(a);
                 }
                 break;
             case SECURITY3:
@@ -176,7 +178,7 @@ public class MyEngine extends Engine{
                         }
                         j++;
                     }} else {
-                    servicePoints[8].AddToTheLine(a);
+                    servicePoints[4].AddToTheLine(a);
                 }
                 break;
             case SECURITY4:
@@ -196,30 +198,30 @@ public class MyEngine extends Engine{
                         }
                         j++;
                     }} else {
-                    servicePoints[8].AddToTheLine(a);
+                    servicePoints[4].AddToTheLine(a);
                 }
                 break;
-            case TICKET_SELL1:
-                a = (Customer) servicePoints[4].TakeFromTheLine(); //Customer moves from Tickets to checkin
-                if(!a.isVipCustomer()) {
+            case SECURITY5:
+                a = (Customer) servicePoints[4].TakeFromTheLine(); //Customer moves from security to tickets
+                if(a.isVipCustomer()) {
                     linecheck = true;
                     j=0;
                     while (linecheck) {
 
-                        for (int i = 10; i < (10+settings[2]); i++) {
+                        for (int i = 5; i < (5+settings[1]); i++) {
                             if (servicePoints[i].GetLineSize() == j) {
                                 servicePoints[i].AddToTheLine(a);
-                                System.out.println("Customer added to Ticket Sells number" + i);
+                                System.out.println("Customer Added to Security number" + i);
                                 linecheck = false;
                                 break;
                             }
                         }
                         j++;
                     }} else {
-                    servicePoints[8].AddToTheLine(a);
+                    servicePoints[9].AddToTheLine(a);
                 }
                 break;
-            case TICKET_SELL2:
+            case TICKET_SELL1:
                 a = (Customer) servicePoints[5].TakeFromTheLine(); //Customer moves from Tickets to checkin
                 if(!a.isVipCustomer()) {
                     linecheck = true;
@@ -236,11 +238,10 @@ public class MyEngine extends Engine{
                         }
                         j++;
                     }} else {
-                    servicePoints[8].AddToTheLine(a);
+                    servicePoints[9].AddToTheLine(a);
                 }
                 break;
-
-            case TICKET_SELL3:
+            case TICKET_SELL2:
                 a = (Customer) servicePoints[6].TakeFromTheLine(); //Customer moves from Tickets to checkin
                 if(!a.isVipCustomer()) {
                     linecheck = true;
@@ -257,12 +258,33 @@ public class MyEngine extends Engine{
                         }
                         j++;
                     }} else {
-                    servicePoints[8].AddToTheLine(a);
+                    servicePoints[9].AddToTheLine(a);
+                }
+                break;
+
+            case TICKET_SELL3:
+                a = (Customer) servicePoints[7].TakeFromTheLine(); //Customer moves from Tickets to checkin
+                if(!a.isVipCustomer()) {
+                    linecheck = true;
+                    j=0;
+                    while (linecheck) {
+
+                        for (int i = 10; i < (10+settings[2]); i++) {
+                            if (servicePoints[i].GetLineSize() == j) {
+                                servicePoints[i].AddToTheLine(a);
+                                System.out.println("Customer added to Ticket Sells number" + i);
+                                linecheck = false;
+                                break;
+                            }
+                        }
+                        j++;
+                    }} else {
+                    servicePoints[9].AddToTheLine(a);
                 }
                 break;
 
             case TICKET_SELL4:
-                a = (Customer) servicePoints[7].TakeFromTheLine(); //Customer moves from Tickets to checkin
+                a = (Customer) servicePoints[8].TakeFromTheLine(); //Customer moves from Tickets to checkin
                 if(!a.isVipCustomer()) {
                 linecheck = true;
                 j=0;
@@ -278,12 +300,12 @@ public class MyEngine extends Engine{
                     }
                     j++;
                 }} else {
-                    servicePoints[8].AddToTheLine(a);
+                    servicePoints[9].AddToTheLine(a);
                 }
                 break;
 
             case CHECK_IN1:
-                a = (Customer) servicePoints[8].TakeFromTheLine(); //Customer moves from checkin to catering
+                a = (Customer) servicePoints[9].TakeFromTheLine(); //Customer moves from checkin to catering
                     linecheck = true;
                     j=0;
                     while (linecheck) {
@@ -300,24 +322,6 @@ public class MyEngine extends Engine{
                 }
                 break;
             case CHECK_IN2:
-                a = (Customer) servicePoints[9].TakeFromTheLine(); //Customer moves from checkin to catering
-                linecheck = true;
-                j=0;
-                while (linecheck) {
-
-                    for (int i = 10; i < (10+settings[3]); i++) {
-                        if (servicePoints[i].GetLineSize() == j) {
-                            servicePoints[i].AddToTheLine(a);
-                            System.out.println("Customer Added to Check In number" + i);
-                            linecheck = false;
-                            break;
-                        }
-                    }
-                    j++;
-                }
-                break;
-
-            case CHECK_IN3:
                 a = (Customer) servicePoints[10].TakeFromTheLine(); //Customer moves from checkin to catering
                 linecheck = true;
                 j=0;
@@ -335,7 +339,7 @@ public class MyEngine extends Engine{
                 }
                 break;
 
-            case CHECK_IN4:
+            case CHECK_IN3:
                 a = (Customer) servicePoints[11].TakeFromTheLine(); //Customer moves from checkin to catering
                 linecheck = true;
                 j=0;
@@ -353,7 +357,7 @@ public class MyEngine extends Engine{
                 }
                 break;
 
-            case CHECK_IN5:
+            case CHECK_IN4:
                 a = (Customer) servicePoints[12].TakeFromTheLine(); //Customer moves from checkin to catering
                 linecheck = true;
                 j=0;
@@ -370,41 +374,59 @@ public class MyEngine extends Engine{
                     j++;
                 }
                 break;
+
+            case CHECK_IN5:
+                a = (Customer) servicePoints[13].TakeFromTheLine(); //Customer moves from checkin to catering
+                linecheck = true;
+                j=0;
+                while (linecheck) {
+
+                    for (int i = 10; i < (10+settings[3]); i++) {
+                        if (servicePoints[i].GetLineSize() == j) {
+                            servicePoints[i].AddToTheLine(a);
+                            System.out.println("Customer Added to Check In number" + i);
+                            linecheck = false;
+                            break;
+                        }
+                    }
+                    j++;
+                }
+                break;
             case CATERING1:
-                a = (Customer) servicePoints[13].TakeFromTheLine();
+                a = (Customer) servicePoints[14].TakeFromTheLine();
                 if (!a.isVipCustomer()) {
-                    servicePoints[17].AddToTheLine(a);
-                } else {
                     servicePoints[18].AddToTheLine(a);
+                } else {
+                    servicePoints[19].AddToTheLine(a);
                 }
                 break;
             case CATERING2:
-                a = (Customer) servicePoints[14].TakeFromTheLine();
+                a = (Customer) servicePoints[15].TakeFromTheLine();
                 if (!a.isVipCustomer()) {
-                    servicePoints[17].AddToTheLine(a);
-                } else {
                     servicePoints[18].AddToTheLine(a);
+                } else {
+                    servicePoints[19].AddToTheLine(a);
                 }
                 break;
             case CATERING3:
-                a = (Customer) servicePoints[15].TakeFromTheLine();
+                a = (Customer) servicePoints[16].TakeFromTheLine();
                 if (!a.isVipCustomer()) {
-                    servicePoints[17].AddToTheLine(a);
-                } else {
                     servicePoints[18].AddToTheLine(a);
+                } else {
+                    servicePoints[19].AddToTheLine(a);
                 }
                 break;
             case CATERING4:
-                a = (Customer) servicePoints[16].TakeFromTheLine();
+                a = (Customer) servicePoints[17].TakeFromTheLine();
                 if (!a.isVipCustomer()) {
-                    servicePoints[17].AddToTheLine(a);
-                } else {
                     servicePoints[18].AddToTheLine(a);
+                } else {
+                    servicePoints[19].AddToTheLine(a);
                 }
                 break;
 
             case CUSTOMER_ARRIVAL:
-                a = (Customer) servicePoints[17].TakeFromTheLine(); // Customer Removed from Simulator
+                a = (Customer) servicePoints[18].TakeFromTheLine(); // Customer Removed from Simulator
                 a.setFinistime(Clock.getInstance().getClock());
                 e = new CustomerDb(a, this.ResultDb.getId());
                 CustomerDao.persist(e);
@@ -418,7 +440,7 @@ public class MyEngine extends Engine{
                 break;
 
             case VIP_CUSTOMER_ARRIVAL:
-                a = (Customer) servicePoints[18].TakeFromTheLine(); // Customer Removed from Simulator
+                a = (Customer) servicePoints[19].TakeFromTheLine(); // Customer Removed from Simulator
                 a.setFinistime(Clock.getInstance().getClock());
                 e = new CustomerDb(a, this.ResultDb.getId());
                 CustomerDao.persist(e);
